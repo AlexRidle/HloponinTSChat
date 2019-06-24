dbea92994f7163e3ea98ad5ff76469b11c1b224b
Alexander <alexanderbely98@gmail.com>
2019-06-24 16:46:05 +0300

    Update ClientAgentStart.java
    Code Review

--- ClientApp/src/main/java/com/clientapp/ClientAgentStart.java
+++ ClientApp/src/main/java/com/clientapp/ClientAgentStart.java
@@ -4,8 +4,8 @@ package com.clientapp;
  * Class will start the console client application
  * @author Paul Hloponin
  */
-public class ClientAgentStart {
+public class ClientAgentStart {//Better to name this class "StartConsoleApp"
     public static void main(String[] args) {
-        new Client("localhost", 2121);
+        new Client("localhost", 2121);//Host and port should be located in properties file.
     }
 }
