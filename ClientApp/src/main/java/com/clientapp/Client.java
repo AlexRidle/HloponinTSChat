7bdc9db6d09616e7355c855283f910ea972120e3
Alexander <alexanderbely98@gmail.com>
2019-06-24 16:45:25 +0300

    Update Client.java
    Code Review

--- ClientApp/src/main/java/com/clientapp/Client.java
+++ ClientApp/src/main/java/com/clientapp/Client.java
@@ -1,6 +1,6 @@
 package com.clientapp;
 
-import java.io.*;
+import java.io.*;//Do not import all classes. Import only what you need.
 import java.net.Socket;
 import java.util.logging.Logger;
 
@@ -12,8 +12,8 @@ public class Client {
     private Socket socket;
     private BufferedReader in;
     private BufferedWriter out;
-    private final String host;
-    private final int port;
+    private final String host;//This variable is never accessed.
+    private final int port;//This variable is never accessed.
 
     public Client(String host, int port) {
         this.host = host;
@@ -25,10 +25,11 @@ public class Client {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         } catch (IOException e) {
-            logger.warning("Connect with server failed");
+            logger.warning("Connect with server failed");//Why do we not logging stack trace? This message is better to show to client.
             System.exit(0);
         }
 
+        //Better to use ExecutorService to run threads.
         new Thread(new Send(socket, out, logger)).start();  //Поток отсылки сообщения
         new Thread(new Receive(socket, in, logger)).start(); //Поток принятия сообщения
     }
